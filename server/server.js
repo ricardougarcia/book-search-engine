a0f135dae0926761ac7d1f5348a52d4f1f25eeec
Ricardo Garcia <ricardougarcia@gmail.com>
2021-12-28 15:19:14 -0500

    add server js app.get line 33

--- server/server.js
+++ server/server.js
@@ -29,7 +29,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
