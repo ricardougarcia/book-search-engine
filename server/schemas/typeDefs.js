b0cbd387fc4c53ee718e1476ec2171ae8a5a833d
Ricardo Garcia <ricardougarcia@gmail.com>
2021-12-28 14:00:35 -0500

    added User typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,12 +11,12 @@ const typeDefs = gql`
     title: String!
   }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
   }
 
   type Query {
